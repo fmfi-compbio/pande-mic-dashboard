a27cd87fae4a05a50a2b2325837a3bc84256c139
janka <14cernikova@gmail.com>
2022-05-15 17:28:10 +0200

    first commit - Flask app dashboard

---
+++ dashboard.py
@@ -0,0 +1,310 @@
+from flask import Flask, render_template, send_from_directory, request, Response
+
+import matplotlib
+import numpy as np
+
+from datastore import Datastore
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+
+import seaborn as sns
+
+import plotly
+import plotly.graph_objects as go
+
+import os
+
+import argparse
+parser = argparse.ArgumentParser(description='Flask-app dashboard for pande-mic.')
+parser.add_argument('--summary_path', help='full path to summary directory', required=True)
+parser.add_argument('--config_path', help='full path to config directory', required=True)
+parser.add_argument('--run_name', help='run name or ID', default="unknown", required=True)
+parser.add_argument('--smoothing_window_size', help='window size used for smoothing of coverage graphs', default="10")
+parser.add_argument('--smoothing_window_step', help='step between smoothing windows for coverage graphs (calculating madians for each position might be slow)', default="10")
+parser.add_argument('--low_cutoff', help='coverage value that is considered too low', default = "20")
+parser.add_argument('--high_cutoff', help='coverage value that is considered too high', default = "3000")
+parser.add_argument('--coverage_cutoff', help='cut values higher than high_cutoff from the graph', dest='coverage_cutoff', action='store_true')
+parser.add_argument('--port', default = 5000, required=False)
+parser.add_argument('--debug', help='run app in debug mode', action='store_true', required=False)
+args = parser.parse_args()
+
+
+summary_full_path = args.summary_path
+config_full_path = args.config_path
+run_name = args.run_name
+smooth = int(args.smoothing_window_size)
+smooth_step = int(args.smoothing_window_step)
+cutoff_low = int(args.low_cutoff)
+cutoff_high = int(args.high_cutoff)
+coverage_cutoff_isset = False
+if args.coverage_cutoff:
+    coverage_cutoff_isset = True
+debug = False
+if args.debug:
+    debug = True
+specified_port = args.port
+
+app = Flask(__name__)
+
+datastore = Datastore(summary_full_path, config_full_path, run_name, smooth, smooth_step)
+
+@app.route('/')
+def index():
+    """render the index page"""
+    global datastore
+    if(os.path.isfile(datastore.summary_path+"basecalled/count.csv")): # if the summary file from base calling exists
+        datastore.read_stats()
+        return render_template('index.html',run_name=datastore.get_run_name())
+    else: # no data yet
+        return render_template('index_preload.html', run_name=datastore.get_run_name())
+
+@app.route('/stats')
+def stats():
+    """read number of reads and bases and render template"""
+    global datastore
+    datastore.read_stats()
+    return render_template('stats.html', reads=datastore.get_num_of_reads() , bases=datastore.get_num_of_bases())
+
+@app.route('/variants_table')
+def variants_table():
+    """get variant summary and render template with variants table"""
+    return render_template('variants_table.html', variants=datastore.get_variant_summary())
+
+@app.route('/summary_graphs')
+def summary_graphs():
+    """read the summary statistics for all barcodes and render the graphs in the summary part of the page"""
+    global datastore
+    return render_template('summary.html', barcodes=datastore.get_barcodes(), colors=datastore.get_colors(), processed_reads=datastore.get_processed_reads())
+
+@app.route('/force_reload_summary_data')
+def force_reload_summary_data():
+    """force reload of summary data"""
+    global datastore
+    datastore.read_stats()
+    datastore.read_base_counts()
+    datastore.read_barcode_summary()
+    datastore.read_variants()
+    datastore.variant_counts()
+    return Response("OK", status=200)
+
+@app.route('/barcodes')
+def barcodes():
+    """get data for individual barcodes and render the template containing list of them"""
+    global datastore
+    return render_template('barcodes.html', barcodes=datastore.get_barcodes(), colors=datastore.get_colors(), processed_reads=datastore.get_processed_reads(), mapped_reads=datastore.get_mapped_reads(), avg_read_lengths = datastore.get_avg_read_lengths())
+
+
+@app.route('/summary_file/<path:fname>')
+def static_file(fname):
+    """load static file from the server"""
+    global datastore
+    return send_from_directory(datastore.summary_path, fname)
+
+@app.route('/read_depth_plotly')
+def read_depth_plotly():
+    """draw a coverage graph using plotly library"""
+    global datastore
+
+    def draw_amplicons():
+        amplicon_pools = datastore.divide_amplicons_into_pools()
+
+        row = 0
+        for pool in amplicon_pools:
+            row+=1
+            for amplicon in pool:
+                top_y = (min_value-row*padding) 
+                bottom_y = (min_value-(row+1)*padding)
+                left_x = amplicon[0]
+                right_x = amplicon[1]
+                fig.add_shape(type="rect",
+                    x0=left_x, y0=top_y, x1=right_x, y1=bottom_y,
+                    line=dict(
+                        color="RoyalBlue",
+                        width=1,
+                    ),
+                    fillcolor="RoyalBlue",
+                )
+
+    def coverage_cutoff():
+        nonlocal padding
+        if coverage_cutoff_isset:
+            lowest = min_value
+            highest = min(cutoff_high, max_value+padding)
+            if highest > lowest:
+                padding = (highest - lowest)/50
+                fig.update_yaxes(range=[lowest-4*padding, highest])
+            else: #low coverage (max_value < low coverage threshold)
+                padding = (max_value-min_value)/50
+                fig.update_yaxes(range=[min_value-4*padding, max_value+padding])
+        else:
+            padding = (max_value-min_value)/50
+            fig.update_yaxes(range=[min_value-4*padding, max_value+padding])
+
+    def add_variant_vlines():
+        """add a vertical line to the graph for each SNP found for a variant"""
+        SNPs = datastore.get_variant_SNPs(barcode)
+        for snp in SNPs:
+            pos = snp["position"]
+            fig.add_vline(x=pos, line_color='red', line_width = 0.5)
+
+
+    barcode = request.args.get('barcode', default = None, type = str)
+    if barcode!=None:
+        datastore.reload_barcode_data(barcode)
+    data = datastore.get_coverage_sliding_window_median()
+    barcodes = datastore.get_barcodes()
+    data_colors = datastore.get_colors()
+
+
+    fig = go.Figure()
+    fig.update_layout(
+        title="Coverage per position ("+str(smooth)+" bases median with step "+str(smooth_step)+")",
+        xaxis_title="position",
+        yaxis_title="coverage",
+        legend_title="barcodes",
+        font=dict(
+            family="Arial",
+            size=12,
+            color="Black"
+        ),
+        paper_bgcolor = "rgba(0,0,0,0)",
+        plot_bgcolor="rgba(0,0,0,0)"
+    )
+    
+    fig.update_xaxes(showgrid=True, gridwidth=0.05, gridcolor='#dbdbdb')
+    fig.update_yaxes(showgrid=True, gridwidth=0.05, gridcolor='#dbdbdb')
+    fig.add_hline(y=cutoff_high, line_color="red", line_width = 0.5, line_dash="dash")
+    fig.add_hline(y=cutoff_low, line_color="red", line_width = 0.5, line_dash="dash")
+    
+    min_value = 99999999999
+    max_value = 0
+    if barcode==None: # draw graph containing all barcodes
+        for barcode in barcodes:
+            if barcode!="unclassified" or len(barcodes)==1:
+                d = np.array(data[barcode])
+                x_data,y_data = d.T
+                mi = min(y_data)
+                if mi<min_value:
+                    min_value = mi
+                ma = max(y_data)
+                if ma>max_value:
+                    max_value = ma
+                fig.add_trace(go.Scatter(x=x_data, y=y_data,
+                        mode='lines',
+                        name=barcode,
+                        line=dict(color=data_colors[barcode])))
+    else: # graph only for one barcode
+        d = np.array(data[barcode])
+        x_data,y_data = d.T
+        min_value = min(y_data)
+        max_value = max(y_data)
+        fig.add_trace(go.Scatter(x=x_data, y=y_data,
+                mode='lines',
+                name=barcode,
+                line=dict(color=data_colors[barcode])))
+        add_variant_vlines()
+
+    padding = (max_value-min_value)/50
+    coverage_cutoff()
+    draw_amplicons()
+    
+    return plotly.io.to_html(fig, full_html=False)
+
+
+@app.route('/read_stats')
+def read_stats():
+    """read summary stats"""
+    global datastore
+    datastore.read_stats()
+
+@app.route('/mapped_and_processed.png')    
+def mapped_and_processed_reads():
+    """create figure for mapped and processed reads graph and save it to a png file"""
+    global datastore
+    sorted_barcodes = datastore.get_barcodes()
+    if len(sorted_barcodes)==1:
+        fig_y_size = 2
+    elif len(sorted_barcodes)<96:
+        fig_y_size = 0.2*len(sorted_barcodes)
+    else:
+        fig_y_size = 10
+    fig, ax = plt.subplots(figsize=(5,fig_y_size))
+    draw_mapped_and_processed_reads(ax)
+    fig.tight_layout()
+    fig.savefig("./static/img/mapped_and_processed_reads.png")
+    plt.close()
+    return "/static/img/mapped_and_processed_reads.png"
+
+def draw_mapped_and_processed_reads(ax):
+    """draw a bar plot with num of processed reads and mapped for each sample, read from barcode_summary/summary.csv, aligned/mapped.csv"""
+    global datastore
+    data_colors = datastore.get_colors()
+    sorted_barcodes = datastore.get_barcodes()
+    processed_reads = datastore.get_processed_reads()
+    mapped_reads = datastore.get_mapped_reads()
+    barcodes=[]
+    counts=[]
+    colors=[]
+    counts_mapped=[]
+    colors_mapped=[]
+    for barcode in sorted_barcodes:
+        if barcode!="unclassified" or len(sorted_barcodes)==1:
+            barcodes.append(barcode)
+            counts.append(processed_reads[barcode])
+            counts_mapped.append(mapped_reads[barcode])
+            colors.append('#808080')
+            colors_mapped.append(data_colors[barcode])
+    ax.margins(y=0)
+    ax.barh(barcodes, counts,  color=colors, align='center')
+    ax.barh(barcodes, counts_mapped,  color=colors_mapped, align='center')
+    ax.invert_yaxis()  # labels read top-to-bottom
+    ax.set_yticks(np.arange(len(barcodes)), labels=barcodes, fontsize = 7)
+    ax.set_ylabel('Barcode')
+    ax.set_xlabel('# of reads')
+    ax.set_title('Processed (grey) and mapped reads (color) \n per sample')    
+
+@app.route('/barcode_amplicon.png')
+def barcode_amplicon_graph():
+    """create a figure for barcode - amplicon heatmap and save it to png"""
+    global datastore
+    sorted_barcodes = datastore.get_barcodes()
+    if len(sorted_barcodes)==1:
+        fig_y_size = 3
+    elif len(sorted_barcodes)<96:
+        fig_y_size = 0.3*len(sorted_barcodes)
+    else:
+        fig_y_size = 10
+    fig, ax = plt.subplots(figsize=(5,fig_y_size))
+    draw_barcode_amplicon_graph(fig,ax,50)
+    fig.savefig("./static/img/barcode_amplicon.png")
+    plt.close()
+    return "/static/img/barcode_amplicon.png"
+
+def draw_barcode_amplicon_graph(fig,ax,max_cov):
+    """draw a baecode-amplicon heatmap"""
+    global datastore
+    amplicon_captions, barcode_captions, barcode_amplicon = datastore.create_amplicon_data()
+    colors = ["#000000","#830029","#e40017","#ff8b02","#74b643"]
+    ax = sns.heatmap( barcode_amplicon , linewidths = .5 , cmap = colors, vmin = 0, vmax = max_cov) # .. square = True
+    # Show all ticks and label them with the respective list entries
+    ax.set_yticks(np.arange(len(barcode_captions))+0.5, labels=barcode_captions, fontsize = 10)
+    ax.set_xticks(np.arange(len(amplicon_captions))+0.5, labels=amplicon_captions, fontsize = 10)
+    # Rotate the tick labels and set their alignment.
+    for tick in ax.get_xticklabels():
+        tick.set_rotation(90)
+    for tick in ax.get_yticklabels():
+        tick.set_rotation(0)
+    # Loop over data dimensions and create text annotations.
+    #for i in range(len(barcodes)):
+    #    for j in range(len(amplicons)):
+    #        text = ax.text(j, i, barcode_amplicon[i, j],
+    #                       ha="center", va="center", color="w")
+    ax.set_title("Avg amplicon coverage for barcodes")
+    fig.tight_layout()
+
+if __name__ == "__main__":
+    if debug:
+        app.run(use_reloader=True, debug=True)  
+    else:
+        from waitress import serve
+        serve(app, host="0.0.0.0", port=specified_port)
---
+++ datastore.py
@@ -0,0 +1,358 @@
+import csv
+import json
+import random
+
+import numpy as np
+from statistics import median
+import time
+
+
+class Datastore:
+    def __init__(self, summary_path, config_path, run_name, smoothing_window_size, smoothing_window_step):
+        self.summary_path = summary_path
+        self.config_path = config_path
+        self.sorted_barcodes = []
+        self.barcode_colors = {}
+        self.reads = 0
+        self.bases = 0
+        self.processed_reads = {}
+        self.barcode_bases = {}
+        self.ref_length = 0
+        self.ref = ""
+        self.ref_name = ""
+        self.pos_coverage = {}
+        self.pos_coverage_sliding_window_median = {}
+        self.run_name = run_name
+        self.smoothing_window_size = smoothing_window_size
+        self.smoothing_window_step = smoothing_window_step
+        self.variants = {}
+        self.variant_summary = {}
+        self.avg_read_lengths = {}
+        self.SNPs = {}
+        self.variant_SNPs = {}
+        self.amplicons = []
+        self.amplicon_pools = []
+        self.mapped_reads = {}
+        self.color_picker_pos = 0
+        self.colors = ['#e82c17', '#e8b417', '#2d851d', '#1d857e', '#162b73', '#411a6e', '#6e1a66', '#4f0814', '#3D81B8', '#33995F', '#A8384F', '#C7C557', '#C69453', '#4E2267', '#302A7E', '#1D4258', '#194D1A', '#8244C1', '#5C391F', '#602038', '#67C95E', '#3E55BB', '#267329', '#D98C8C', '#4FC4C0', '#B83DB6', '#090A1B', '#627CCB', '#2A687E']
+
+####################################################################################
+##### BASIC STATS ###########
+####################################################################################
+    
+    def read_stats(self):
+        self.read_ref_from_file()
+        self.read_total_reads_and_bases()
+        self.read_barcode_summary()
+        self.read_mapped_reads()
+
+    def get_run_name(self):
+        return self.run_name
+
+####################################################################################
+
+    def get_ref_length(self, force_reload=False):
+        if self.ref_length == 0 or force_reload:
+            self.read_ref_from_file()
+        return self.ref_length
+
+    def read_ref_from_file(self):
+        with open(self.config_path+"ref.fasta", 'r') as f:
+            self.ref_name = f.readline() #reference genome
+            self.ref = f.readline().strip()[1:]
+            self.ref_length = len(self.ref)
+
+####################################################################################
+
+    def get_num_of_reads(self, force_reload=False):
+        if self.reads == 0 or force_reload:
+            self.read_total_reads_and_bases()
+        return self.reads
+
+    def get_num_of_bases(self, force_reload=False):
+        if self.bases == 0 or force_reload:
+            self.read_total_reads_and_bases()
+        return self.bases
+
+    def read_total_reads_and_bases(self):
+        with open(self.summary_path+"basecalled/count.csv", 'r') as f:
+            line = f.readline()
+            l = line.split(",") #num of reads, num of bases (total)
+            self.reads = int(l[0])
+            self.bases = int(l[1])
+
+####################################################################################
+########### COVERAGE  ###########
+####################################################################################
+
+    def get_pos_coverage(self, force_reload=False):
+        if not self.pos_coverage or force_reload:
+            self.read_base_counts()
+        return self.pos_coverage
+    
+    def get_barcode_pos_coverage(self, barcode, force_reload=False):
+        if not self.pos_coverage or not barcode in self.pos_coverage or force_reload:
+            self.read_base_counts()
+        return self.pos_coverage[barcode]
+
+    def get_coverage_sliding_window_median(self, force_reload=False):
+        if not self.pos_coverage_sliding_window_median or force_reload:
+            self.read_base_counts()
+        return self.pos_coverage_sliding_window_median
+
+
+    def read_base_counts(self):
+        start = time.time()
+        if not self.sorted_barcodes: #dict. empty
+            self.read_barcode_summary()
+        for barcode in self.sorted_barcodes:
+            self.read_base_counts_for_barcode(barcode)
+        end = time.time()
+        print("csv processing finished in:",end-start)
+
+    def read_base_counts_for_barcode(self,barcode):
+        cov, cov_sliding_w= self.read_base_counts_file(barcode+".csv")
+        self.pos_coverage[barcode] = cov
+        self.pos_coverage_sliding_window_median[barcode] = cov_sliding_w
+
+    def read_base_counts_file(self,file):
+        print("processing file "+file+"...")
+        cov = [] #[pos, coverage]
+        cov_sliding_w = []
+        with open(self.summary_path+"coverage_per_base/"+file, 'r') as f:
+            csvreaded = csv.reader(f)
+            header = next(csvreaded) #position, A, C, G, T
+            for line in csvreaded:
+                c = int(line[1])+int(line[2])+int(line[3])+int(line[4])
+                cov.append([int(line[0]), c])
+        cov_sliding_w = self.median_window(cov,self.smoothing_window_size, step = self.smoothing_window_step)
+        return cov, cov_sliding_w
+
+    def median_window(self,data, w_size, step = 1):
+        cov_sliding_w_xb = [] #[pos, soverage for sliding window of length 10]
+        data_length = len(data)
+        for i in range(0, data_length-w_size, step):
+            cov_sliding_w_xb.append([i, median([int(w[1]) for w in data[i:(i+w_size)]])] )
+        return cov_sliding_w_xb
+
+    """
+    def avg_window(self,data, w_size):
+        cov_sliding_w_xb = [] #[pos, soverage for sliding window of length 10]
+        data_length = len(data)
+        for i in range(0, data_length-w_size):
+            cov_sliding_w_xb.append([i, np.average([int(w[1]) for w in data[i:(i+w_size)]])] )
+        return cov_sliding_w_xb
+    """
+
+####################################################################################
+######### BARCODES ###########
+####################################################################################
+
+    def get_barcodes(self, force_reload=False):
+        if len(self.sorted_barcodes)==0 or force_reload:
+            self.read_barcode_summary()
+        return self.sorted_barcodes
+
+    def get_colors(self, force_reload=False):
+        if not self.barcode_colors or force_reload:
+            self.read_barcode_summary()
+        return self.barcode_colors
+
+    def get_processed_reads(self, force_reload=False):
+        if not self.processed_reads or force_reload:
+            self.read_barcode_summary()
+        return self.processed_reads
+
+    def get_mapped_reads(self, force_reload=False):
+        if not self.mapped_reads or force_reload:
+            self.read_mapped_reads()
+        return self.mapped_reads
+
+    def get_avg_read_lengths(self, force_reload=False):
+        if not self.avg_read_lengths or force_reload:
+            self.read_barcode_summary()
+        return self.avg_read_lengths
+
+    def read_barcode_summary(self):
+        with open(self.summary_path+"barcode_summary/summary.csv", 'r') as f:
+            for line in f:
+                line = line.strip()
+                l = line.split(",") # num of reads, num of bases, barcode
+                barcode = l[2]
+                self.add_barcode(barcode)
+                self.processed_reads[barcode]=int(l[0])
+                self.barcode_bases[barcode]=int(l[1])
+                if self.barcode_bases[barcode]!= 0:
+                    self.avg_read_lengths[barcode]=round(float(l[1])/float(l[0]), 1)
+                else:
+                    self.avg_read_lengths[barcode]=0
+
+    def read_mapped_reads(self):
+        with open(self.summary_path+"aligned/mapped.csv", 'r') as f:
+            for line in f:
+                line = line.strip()
+                l = line.split(",") # barcode, num of reads
+                barcode = l[0]
+                self.add_barcode(barcode)
+                self.mapped_reads[barcode]=int(l[1])
+
+    def add_barcode(self,barcode):
+        if not barcode in self.sorted_barcodes: # update color and add barcode only if it is not already there
+            self.sorted_barcodes.append(barcode)
+            self.barcode_colors[barcode] = self.choose_color()#we dont want the colors to be completely random
+
+    def choose_color(self):
+        if self.color_picker_pos>=len(self.colors)-1:
+            color = "#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
+        else:
+            color = self.colors[self.color_picker_pos]
+            self.color_picker_pos += 1 
+        return color
+
+
+#######################################################################
+
+    def reload_barcode_data(self, barcode):
+        self.read_base_counts_for_barcode(barcode)
+        self.read_variant_SNPs_for_barcode(barcode)
+
+    def read_variant_SNPs_for_barcode(self, barcode):
+        self.variant_SNPs[barcode] = []
+        self.recursive_variant_SNPs(self.variants[barcode], barcode)
+
+####################################################################################
+###### VARIANTS ######
+####################################################################################
+
+    def get_variant_summary(self):
+        if not self.variant_summary:
+            self.variant_counts()
+        return self.variant_summary
+
+    def get_variant_SNPs(self, barcode):
+        if not self.variant_SNPs:
+            self.read_variant_SNPs()
+        return self.variant_SNPs[barcode]
+
+    def variant_counts(self):
+        self.variant_summary = {}
+        if not self.variants:
+            self.read_variants()
+        for barcode in self.variants:
+            self.recursive_variant_summary(self.variants[barcode], self.variant_summary, barcode)
+
+    def recursive_variant_summary(self, variants, variant_summary_parent, barcode):
+        for variant in variants:
+            vname = variant["name"]
+            if not vname in variant_summary_parent:
+                variant_summary_parent[vname]={}
+                variant_summary_parent[vname]["count"] = 1
+                variant_summary_parent[vname]["barcodes"] = [barcode]
+                variant_summary_parent[vname]["children"] = {}
+            else:
+                variant_summary_parent[vname]["count"]+=1
+                variant_summary_parent[vname]["barcodes"].append(barcode)
+            
+            self.recursive_variant_summary(variant["subs"], variant_summary_parent[vname]["children"], barcode)
+
+    def read_variant_SNPs(self):
+        for barcode in self.sorted_barcodes:
+            self.variant_SNPs[barcode] = []
+            self.recursive_variant_SNPs(self.variants[barcode], barcode)
+
+    
+    def recursive_variant_SNPs(self, variants, barcode):
+        for variant in variants:
+            vmut = variant["mutations"]
+            for snp in vmut:
+                self.variant_SNPs[barcode].append(snp)
+            self.recursive_variant_SNPs(variant["subs"], barcode)
+
+    def read_variants(self):
+        self.variants = {}
+        for barcode in self.sorted_barcodes:
+            self.variants[barcode] = self.read_variants_for_barcode(barcode)
+
+    def read_variants_for_barcode(self, barcode):
+        with open(self.summary_path+"variants/"+barcode+".json", "r") as read_file:
+            data = json.load(read_file)
+        return data
+
+#######   get_SNPs (unused)  ################################################################
+    """
+    def get_SNPs(self,barcode, force_reload=False):
+        if not self.SNPs:
+            self.read_SNPs()
+        return self.SNPs[barcode]
+    
+    def read_SNPs(self):
+        self.SNPs = {}
+        for barcode in self.sorted_barcodes:
+            self.variants[barcode] = self.read_SNPs_for_barcode(barcode)
+
+    def read_SNPs_for_barcode(self, barcode):
+        with open(self.summary_path+"SNPs/"+barcode+".csv", 'r') as f:
+            self.SNPs[barcode]=[]
+            f.readline()
+            for line in f:
+                l = line.split(",")
+                self.SNPs[barcode].append([l[0], int(l[1]), int(l[2])])
+    """
+
+####################################################################################
+###### AMPLICONS #####
+####################################################################################
+
+    def get_amplicons(self):
+        if len(self.amplicons)==0:
+            return self.read_amplicons()
+        return self.amplicons
+
+    def read_amplicons(self):
+        f = open(self.config_path+'amplicons.json')
+        data = json.load(f)
+        int_start_end = [ [int(a[0]), int(a[1])] for a in data["amplicons"] ]
+        self.amplicons = int_start_end
+        return self.amplicons
+
+    def create_amplicon_data(self,trim=20, ignore_unclassified=True):
+        f = open(self.config_path+'amplicons.json')
+        data = json.load(f)
+        amplicon_captions = [] 
+        for i in range(len(data['amplicons'])):
+            amplicon_captions.append(str(data['amplicons'][i][0])+"-"+str(data['amplicons'][i][1]))
+        amplicons = data['amplicons']
+        if not ignore_unclassified:
+            barcode_captions = self.sorted_barcodes
+        else:
+            barcode_captions = [b for b in self.sorted_barcodes if (not b=="unclassified" or len(self.sorted_barcodes)==1)]
+        barcode_amplicon = np.array([self.avgs_for_amplicons(amplicons, self.pos_coverage[barcode],trim, barcode) for barcode in barcode_captions])
+        f.close()
+        return amplicon_captions, barcode_captions, barcode_amplicon
+
+    def avgs_for_amplicons(self,amplicons, barcode, trim, barcode_name):
+        avg_cov = []
+        for amplicon in amplicons:
+            amp_arr = [v[1] for v in barcode[int(amplicon[0])-1+trim : amplicon[1]-1-trim]]
+            avg_cov.append(sum(amp_arr)/len(amp_arr))
+        return avg_cov
+
+    def divide_amplicons_into_pools(self):
+        if len(self.amplicons) == 0:
+            self.read_amplicons()
+        if len(self.amplicon_pools) == 0:
+            pool = 0
+            prev = [-1,-1]
+            pools = [[]]
+            for amplicon in self.amplicons:
+                if prev[1]<amplicon[0]:
+                    pools[pool].append(amplicon)
+                else:
+                    pools.append([])
+                    pool+=1
+                    pools[pool].append(amplicon)
+                prev = amplicon
+            self.amplicon_pools = pools
+            return pools
+        else:
+            return self.amplicon_pools
